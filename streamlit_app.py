e3ff98727a77aba246f9f6b6fcd35f3ad0d72852
Jaedin Hernandez-Rogers <jaedinihernandez@gmail.com>
2026-02-26 14:58:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ if st.button("Predict"):
 
     st.subheader("Result")
 
-    if probability == 1:
+    if probability > 0.5:
         st.error(f"⚠️ High Injury Risk ({prob:.0%})")
     else:
         st.success(f"✅ Low Injury Risk ({prob:.0%})")
