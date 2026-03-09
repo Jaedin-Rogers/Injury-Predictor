a381a89a66ae67af7678fd0e60c93ea5ca147a1e
Jaedin Hernandez-Rogers <jaedinihernandez@gmail.com>
2026-03-09 17:15:30 -0500

    Implement CORS middleware for API
    Added CORS middleware to allow requests from any origin.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 
 from fastapi.middleware.cors import CORSMiddleware
 
+# Allow all websites, credentials, request types, and headers
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
@@ -13,18 +14,18 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+# Define datatypes for predictors
 class PlayerData(BaseModel):
     training_hours: float
     recovery_days: float
     fatigue_score: float
 
-    
+# Define GET endpoint    
 @app.get("/")
 def home():
     return {"message": "API is working"}
 
-
+# Define prediction endpoint and call ML model
 @app.post("/predict")
 def predict(data: PlayerData):
     result = predict_injury(
