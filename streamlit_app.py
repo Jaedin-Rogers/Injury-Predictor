27f0b7f7ae0f2ec1de0a23a0d4772a2d37600e0c
Jaedin Hernandez-Rogers <jaedinihernandez@gmail.com>
2026-03-09 17:19:54 -0500

    Add injury risk prediction functionality
    Added input fields for training hours, recovery days, and fatigue score. Implemented prediction functionality with API integration and risk classification.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,14 @@ import requests
 
 st.title("Injury Risk Predictor")
 
+# Define predictor imput boxes and data type
 training_hours = st.number_input("Training Hours")
 recovery_days = st.number_input("Recovery Days")
 fatigue_score = st.number_input("Fatigue Score(1-10)")
 
+# Add "Predict" button and create dictionary for predictors
 if st.button("Predict"):
+    # Create endpoint and send data to API
     res = requests.post(
         "https://injury-predictor-api-production.up.railway.app/predict",
         json={
@@ -16,6 +19,7 @@ if st.button("Predict"):
             "fatigue_score":fatigue_score
         }
     )
+    # Convert response to JSON and extract values
     result = res.json()
     probability = result["probability"]
     risk = result["risk"]
@@ -23,6 +27,7 @@ if st.button("Predict"):
 
     st.subheader("Result")
 
+    # Risk classification and warnings
     if probability > 0.5:
         st.error(f"⚠️ High Injury Risk ({prob:.0%})")
     else:
